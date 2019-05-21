ccb11b045b5f9bab67f954c995db3f63d1a29ddf
user <user@es301n04>
2019-05-21 09:44:26 +0800

    464

---
+++ justin/src/com/justin/My.java
@@ -0,0 +1,32 @@
+package com.justin;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class My {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+			FileInputStream fis =new FileInputStream("result.txt");
+			int n =fis.read();
+			try {
+			while(n!=-1) {
+				System.out.println((char)n);
+				n=fis.read();
+			}
+			}catch(Exception e ) {
+				
+			}finally {
+				if(fis!=null ) {
+					try {
+					fis.close();
+				}catch(Exception e) {
+					
+				}
+				}
+			}
+			}
+	}
+
+	
+
